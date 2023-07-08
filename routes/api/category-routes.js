e3d8afb511b0d3f4be9a200077fbf3e015b16042
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-07-08 09:01:04 -0600

    Created an update route for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -59,7 +59,29 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+.then(categoryData => {
+  if(!categoryData){
+    res.status(404).json({alert: 'Id does not match with existing Categories'});
+    return;
+  }
+  res.json(categoryData)
+})
+.catch(err => {
+  console.log(err);
+  res.status(505).json(err)
 });
+});
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
